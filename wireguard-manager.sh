b4c0fe89047a11960df7537569d45677a262b8b4
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-12-18 06:40:49 -0500

    Update wireguard-manager.sh

--- wireguard-manager.sh
+++ wireguard-manager.sh
@@ -905,9 +905,8 @@ if [ ! -f "${WIREGUARD_CONFIG}" ]; then
       fi
       unbound-anchor -a ${UNBOUND_ANCHOR}
       curl "${UNBOUND_ROOT_SERVER_CONFIG_URL}" --create-dirs -o ${UNBOUND_ROOT_HINTS}
-      NPROC=$(nproc)
       echo "server:
-    num-threads: ${NPROC}
+    num-threads: $(nproc)
     verbosity: 1
     root-hints: ${UNBOUND_ROOT_HINTS}
     auto-trust-anchor-file: ${UNBOUND_ANCHOR}
