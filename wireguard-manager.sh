e06bc301cbea47f25d7af9d685d3fb6e1c81acfb
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-01-23 15:32:43 -0500

    Better way to check if the string is empty.

--- wireguard-manager.sh
+++ wireguard-manager.sh
@@ -1159,8 +1159,8 @@ else
       MTU_CHOICE=$(head -n1 ${WIREGUARD_CONFIG} | awk '{print $7}')
       NAT_CHOICE=$(head -n1 ${WIREGUARD_CONFIG} | awk '{print $8}')
       CLIENT_ALLOWED_IP=$(head -n1 ${WIREGUARD_CONFIG} | awk '{print $9}')
-      # Check if an unused IP is found.
-      if [[ "${UNUSED_IPS}" == "" ]]; then
+      # Check for any unused IP addresses.
+      if [ "${#UNUSED_IPS}" -eq 0 ]; then
         CLIENT_ADDRESS_V4=$(echo "${PRIVATE_SUBNET_V4}" | cut -d'.' -f1-3).$((LASTIPV4 + 1))
         CLIENT_ADDRESS_V6="${PRIVATE_SUBNET_V6::-3}$((LASTIPV6 + 1))"
       else
